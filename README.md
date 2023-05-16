4f2e70b5605f2316ac7d35b53c0720e1161f80b6
Becky Pennock <becky.pennock@clearlink.com>
2023-05-16 08:07:36 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Starter for Netlify Workflow
+
+I like to make microsites and vignettes to try out new tech and silly ideas. This is a base for that.
+
+## Create Things
+`npm run build` will deploy things FOR SCIENCE (assuming react.js as a base)
+
+## Modify with a non-react app
+- Replace react with whatever is needed
+- Update the build/directories within netlifi.toml
+- Make sure that data is synced with your nelify site settings
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Biki's Baby Netlify Boilerplate
+        </p>
+        <a
+          className="App-link"
+          href="http://github.com/jeanox"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit biki's github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
